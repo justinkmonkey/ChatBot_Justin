ab53b28c12c9c1cd096e6cc89ec33ca9efaf4249
justinkmonkey <justinkmonkey@hotmail.com>
2026-06-21 11:31:46 -0500

    we added nextjs as front end now make it a chatbot and link with python

--- fun.py
+++ fun.py
@@ -31,7 +31,7 @@ def gemini_chatbot_response(user_input):
 
     client = genai.Client(api_key=GEMINI_API_KEY)
     response = client.models.generate_content(
-        model="gemini-2.0-flash",
+        model="gemini-2.5-flash",
         contents=user_input
     )
     return response.text
